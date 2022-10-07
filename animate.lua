8aa4dd3c62931b3ec38474be5cfc5c78927d545f
redpawed <112795569+redpawed@users.noreply.github.com>
2022-10-07 14:37:37 +1100

    Update animate.lua

--- animate.lua
+++ animate.lua
@@ -1,8 +1,21 @@
 local animate = {}
+animate.easingstyles = {}
 
-local runs = game:GetService"RunService"
+function animate.easingstyles.easeinoutelastic (a)
+	local c5 = 2 * math.pi / 4.5
 
-function easeinoutcubic (a)
+	if a == 0 then
+		return 0
+	elseif a == 1 then
+		return 1
+	elseif a < 0.5 then
+		return -(math.pow(2, 20 * a - 10) * math.sin((20 * a - 11.125) * c5)) / 2
+	else
+		return (math.pow(2, -20 * a + 10) * math.sin((20 * a - 11.125) * c5)) / 2 + 1
+	end
+end
+
+function animate.easingstyles.easeinoutcubic (a)
 	if a < 0.5 then
 		return 4 * a * a * a
 	else
@@ -10,32 +23,21 @@ function easeinoutcubic (a)
 	end
 end
 
+
+local runs = game:GetService"RunService"
+
 function animate.lerpjoint (joint, c0, c1, t)
 	local currentt = 0
 
 	local start0 = joint.C0
 	local start1 = joint.C1
-	
-	task.spawn(function()
-		while currentt <= t do
-			currentt = currentt + runs.Heartbeat:Wait()
-			
-			joint.C0 = start0:Lerp(c0, easeinoutcubic(currentt/t))
-			joint.C1 = start1:Lerp(c1, easeinoutcubic(currentt/t))
-		end
-	end)
-end
-
-function animate.lerppart (part, cframe, t)
-	local currentt = 0
-
-	local start = part.CFrame
 
 	task.spawn(function()
 		while currentt <= t do
 			currentt = currentt + runs.Heartbeat:Wait()
 
-			part.CFrame = start:Lerp(cframe, currentt/t)
+			joint.C0 = start0:Lerp(c0, animate.easingstyles.easeinoutcubic(currentt/t))
+			joint.C1 = start1:Lerp(c1, animate.easingstyles.easeinoutcubic(currentt/t))
 		end
 	end)
 end
